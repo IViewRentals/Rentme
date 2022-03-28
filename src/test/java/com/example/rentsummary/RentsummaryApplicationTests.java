b75c80889434227c4b5c86bd5d3dc4eab6d38a89
huangsheng.415 <huangsheng.415@bytedance.com>
2022-03-29 02:28:22 +0800

    add method to test connection with database

--- src/test/java/com/example/rentsummary/RentsummaryApplicationTests.java
+++ src/test/java/com/example/rentsummary/RentsummaryApplicationTests.java
@@ -1,14 +1,27 @@
 package com.example.rentsummary;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
 
 @SpringBootTest
 class RentsummaryApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     void contextLoads() {
 
+    }
+
+    @Test
+    void getConnection() throws SQLException {
+        System.out.println(dataSource.getConnection());
     }
 
 }
