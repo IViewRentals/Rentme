e786b38ce0e1e8af84cf82dc426287718be25be4
little-white12 <17736138617@163.com>
2022-03-31 22:06:21 +0800

    change control function
    change control function

--- src/main/java/com/example/rentsummary/controller/UserController.java
+++ src/main/java/com/example/rentsummary/controller/UserController.java
@@ -1,32 +1,71 @@
 package com.example.rentsummary.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.example.rentsummary.model.AreaEntity;
-import com.example.rentsummary.model.CityEntity;
-import com.example.rentsummary.model.StateEntity;
-import com.example.rentsummary.model.UserEntity;
+import com.example.rentsummary.model.*;
 import com.example.rentsummary.server.UserService;
+import com.example.rentsummary.server.VerifyCodeGenService;
+import com.example.rentsummary.server.impl.SimpleCharVerifyCodeGenImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
 public class UserController {
-
+    private static String genCode;
     @Autowired
     private UserService userService;
 
+    @GetMapping("/verifyCode")
+    public void verifyCode(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            VerifyCodeGenService verifyCodeGenService = new SimpleCharVerifyCodeGenImpl();
+            //设置长宽
+            VerifyCode verifyCode = verifyCodeGenService.generate(80, 28);
+            String code = verifyCode.getCode();
+            genCode = code;
+            //将VerifyCode绑定session
+            request.getSession().setAttribute("VerifyCode", code);
+            //设置响应头
+            response.setHeader("Pragma", "no-cache");
+            //设置响应头
+            response.setHeader("Cache-Control", "no-cache");
+            //在代理服务器端防止缓冲
+            response.setDateHeader("Expires", 0);
+            //设置响应内容类型
+            response.setContentType("image/jpeg");
+            response.getOutputStream().write(verifyCode.getImgBytes());
+            response.getOutputStream().flush();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @ResponseBody
     @PostMapping(value = "register")
-    public String register(UserEntity userEntity) {
+    public String register(UserEntity userEntity, String verifyCode) {
         JSONObject result = new JSONObject();
         if (userEntity == null) { // the data is empty
             result.put("flag", 0);
             result.put("msg", "Please input form!");
+            return result.toString();
+        }
+        if (!verifyCode.equalsIgnoreCase(genCode)) {
+            result.put("flag", 0);
+            result.put("msg", "Verification code error!");
+            return result.toString();
+        }
+        List<UserEntity> user = userService.getUserByAccount(userEntity.getAccount());
+        if (user.size() > 0) {
+            // 用户名已存在
+            result.put("flag", 0);
+            result.put("msg", "The user name already exists!");
             return result.toString();
         }
 
@@ -42,11 +81,16 @@ public class UserController {
 
     @ResponseBody
     @GetMapping(value = "login")
-    public String login(UserEntity userEntity) {
+    public String login(UserEntity userEntity,String verifyCode) {
         JSONObject result = new JSONObject();
         if (userEntity == null) {
             result.put("flag", 0);
             result.put("msg", "Please input form!");
+            return result.toString();
+        }
+        if (!verifyCode.equalsIgnoreCase(genCode)) {
+            result.put("flag", 0);
+            result.put("msg", "Verification code error!");
             return result.toString();
         }
         UserEntity user = userService.login(userEntity);
--- src/main/java/com/example/rentsummary/dao/UserDAO.java
+++ src/main/java/com/example/rentsummary/dao/UserDAO.java
@@ -10,19 +10,57 @@ import java.util.List;
 public interface UserDAO {
 
 
-    // user login
+    /**
+     * user login
+     * @param userEntity
+     * @return
+     */
     UserEntity login(UserEntity userEntity);
 
-    // user register
+    /**
+     * user register
+     * @param userEntity
+     * @return
+     */
     int register(UserEntity userEntity);
 
+    /**
+     * 获取州
+     * @return
+     */
     List<StateEntity> getState();
 
+    /**
+     * 根据ID获取城市
+     * @param id
+     * @return
+     */
     List<CityEntity> getCityById(int id);
 
+    /**
+     * 添加用户搜索记录
+     * @param userEntity
+     */
     void addUserHistory(UserEntity userEntity);
 
+    /**
+     * 根据ID获取用户
+     * @param id
+     * @return
+     */
     UserEntity getUserById(int id);
 
+    /**
+     * 根据ID获取区域
+     * @param id
+     * @return
+     */
     List<AreaEntity> getAreaById(int id);
+
+    /**
+     * 根据账号获取用户（判断用户名是否重复）
+     * @param account
+     * @return
+     */
+    List<UserEntity> getUserByAccount(String account);
 }
